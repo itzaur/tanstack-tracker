c1459bc8f7c64d71df872ebed87575686a83ce16
itzaur <preneur85@gmail.com>
2025-06-26 16:09:07 +0300

    feat: render the recent transactions card

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { RecentTransaction } from './-recent-transaction';
+import { getRecentTransactions } from '@/data/getRecentTransactions';
 
 export const Route = createFileRoute('/_authed/dashboard/')({
   component: RouteComponent,
+  loader: async () => {
+    const transactions = await getRecentTransactions();
+
+    return { transactions };
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/"!</div>;
+  const { transactions } = Route.useLoaderData();
+
+  return (
+    <div className='max-w-screen-xl mx-auto py-5'>
+      <h1 className='text-4xl font-semibold pb-5'>Dashboard</h1>
+      <RecentTransaction transactions={transactions} />
+    </div>
+  );
 }
