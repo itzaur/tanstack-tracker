2398a34b946aa4326c0eb6c2cc740527c731017c
itzaur <preneur85@gmail.com>
2025-07-31 13:54:46 +0300

    feat: improve the bar chart UI

--- app/routes/_authed/dashboard/-cashflow.tsx
+++ app/routes/_authed/dashboard/-cashflow.tsx
@@ -1,5 +1,9 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { ChartContainer } from '@/components/ui/chart';
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
 import {
   Select,
   SelectContent,
@@ -7,8 +11,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { Value } from '@radix-ui/react-select';
 import { useNavigate } from '@tanstack/react-router';
-import { Bar, BarChart } from 'recharts';
+import { format } from 'date-fns';
+import numeral from 'numeral';
+import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
 
 export function Cashflow({
   yearsRange,
@@ -67,6 +74,35 @@ export function Cashflow({
           className='w-full h-[300px]'
         >
           <BarChart data={annualCashflow}>
+            <CartesianGrid vertical={false} />
+            <YAxis
+              tickFormatter={(value) => {
+                return `$${numeral(value).format('0,0')}`;
+              }}
+            />
+            <XAxis
+              dataKey='month'
+              tickFormatter={(value) => {
+                return format(new Date(year, value - 1, 1), 'MMM');
+              }}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  labelFormatter={(value, payload) => {
+                    return (
+                      <div>
+                        {format(
+                          new Date(year, payload[0]?.payload?.month - 1, 1),
+                          'MMM'
+                        )}
+                      </div>
+                    );
+                  }}
+                />
+              }
+            />
+            <Legend align='right' verticalAlign='top' />
             <Bar dataKey='income' fill='var(--color-income)' radius={4} />
             <Bar dataKey='expenses' fill='var(--color-expenses)' radius={4} />
           </BarChart>
